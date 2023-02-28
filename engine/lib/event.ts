617edbd1378aa13d1cd64b06ac3a16842343f03c
Vahelnir <vahelnir@gmail.com>
2023-02-28 10:29:54 +0100

    feat: batch events before sending them

--- engine/lib/event.ts
+++ engine/lib/event.ts
@@ -1,9 +1,24 @@
+import { debounce } from "throttle-debounce";
+
 interface EventTypes {
   click: { selector: string; innerText: string };
   resize: { width: number; height: number };
 }
 
-export function emit<T extends keyof EventTypes>(name: T, data: EventTypes[T]) {
-  console.log("sending event", name, "with data", data);
-  // fetch("");
+let eventBatch: unknown[] = [];
+
+export function emit<T extends keyof EventTypes>(
+  event: T,
+  data: EventTypes[T]
+) {
+  eventBatch.push({ event, ...data, time: Date.now() });
+  sendEventBatch();
+}
+
+const sendEventBatch = debounce(500, rawSendEventBatch);
+
+function rawSendEventBatch() {
+  console.log("sending batch", eventBatch);
+  eventBatch = [];
+  // fetch()
 }
