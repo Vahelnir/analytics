dc01fa0734f22f5cc8e2651e076fe1359ab764a9
Vahelnir <vahelnir@gmail.com>
2023-02-28 15:02:51 +0100

    feat(backend): build with declaration types

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,7 @@
 import { createServer } from "./server";
 
+export type { AppRouter } from "./router";
+
 async function run() {
   const server = createServer({
     database: "mongodb://127.0.0.1:27017/analytics",
