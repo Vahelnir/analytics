1afa5d5af18f2e025f3a935950138511b61b5c6e
Vahelnir <vahelnir@gmail.com>
2023-03-05 00:09:26 +0100

    feat(frontend): pause requests when refreshing token

--- frontend/src/trpc.ts
+++ frontend/src/trpc.ts
@@ -1,14 +1,15 @@
 import type { AppRouter } from "backend/types/trpc/router";
-import { observable } from "@trpc/server/observable";
+import { Observable, observable } from "@trpc/server/observable";
 import { createTRPCReact, httpBatchLink, TRPCLink } from "@trpc/react-query";
 import superjson from "superjson";
 import { AuthTokens, getAuthTokens, setAuthTokens } from "./service/auth";
+import { router } from "./router";
 
 export const trpc = createTRPCReact<AppRouter>();
 export const trpcClient = trpc.createClient({
   transformer: superjson,
   links: [
-    refetchLink(),
+    authLink({ ignore: ["api.user.refresh"], redirect: "/login" }),
     httpBatchLink({
       url: "http://localhost:3000/trpc",
       headers() {
@@ -25,56 +26,70 @@ export const trpcClient = trpc.createClient({
   ],
 });
 
-function refetchLink(): TRPCLink<AppRouter> {
+function authLink({
+  ignore,
+  redirect,
+}: {
+  ignore: string[];
+  redirect: string;
+}): TRPCLink<AppRouter> {
   return () => {
-    // here we just got initialized in the app - this happens once per app
-    // useful for storing cache for instance
+    let isRefreshing = false;
+    let currentRefresh$: Observable<unknown, unknown> | null = null;
+    async function refreshToken() {
+      const { refreshToken: currentRefreshToken } = getAuthTokens();
+      if (!currentRefreshToken) {
+        return router.navigate(redirect);
+      }
+
+      console.log("refreshing the tokens");
+      // cast as a type because TRPC currently has broken
+      // types with the raw client when using the react client
+      isRefreshing = true;
+      const tokens = await (
+        trpcClient.mutation as (
+          path: string,
+          input: Record<string, string>
+        ) => Promise<AuthTokens>
+      )("api.user.refresh", { refreshToken: currentRefreshToken });
+      setAuthTokens(tokens);
+      isRefreshing = false;
+      currentRefresh$ = null;
+    }
+
     return ({ next, op }) => {
-      // this is when passing the result to the next link
-      // each link needs to return an observable which propagates results
-      return observable((observer) => {
-        console.log("performing operation:", op);
-        const unsubscribe = next(op).subscribe({
-          next(value) {
-            console.log("we received value", value);
-            observer.next(value);
-          },
-          async error(error) {
-            if (error.data?.code !== "UNAUTHORIZED") {
+      function performRequest() {
+        return observable((observer) => {
+          const unsubscribe = next(op).subscribe({
+            next: observer.next,
+            complete: observer.complete,
+            async error(error) {
+              if (error.data?.code === "UNAUTHORIZED" && !isRefreshing) {
+                refreshToken();
+              }
               observer.error(error);
-              return;
-            }
-
-            const { refreshToken: currentRefreshToken } = getAuthTokens();
-            console.log(
-              "we received error",
-              error,
-              "refresh:",
-              currentRefreshToken
-            );
-            if (!currentRefreshToken) {
-              observer.error(error);
-              return;
-            }
-
-            console.log("should refetch");
-            // cast as a type because TRPC currently has broken
-            // types with the raw client when using the react client
-            const tokens = await (
-              trpcClient.mutation as (
-                path: string,
-                input: Record<string, string>
-              ) => Promise<AuthTokens>
-            )("api.user.refresh", { refreshToken: currentRefreshToken });
-            setAuthTokens(tokens);
-            observer.error(error);
-          },
-          complete() {
-            observer.complete();
-          },
+            },
+          });
+          return unsubscribe;
         });
-        return unsubscribe;
-      });
+      }
+
+      // allow some requests to not wait the refresh
+      if (ignore.includes(op.path)) {
+        currentRefresh$ = performRequest();
+        return currentRefresh$;
+      }
+
+      // wait for the current refresh to be done
+      if (isRefreshing && currentRefresh$) {
+        return observable((observer) =>
+          currentRefresh$?.subscribe({
+            complete: () => next(op).subscribe(observer),
+          })
+        );
+      }
+
+      return performRequest();
     };
   };
 }
